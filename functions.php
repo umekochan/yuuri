d31dd1be705ae151ae84cdcd440d5a585339e8d6
umekochan <masuda.ff@gmail.com>
2023-08-26 04:57:18 +0900

    ログイン処理の実装

--- functions.php
+++ functions.php
@@ -1,8 +1,68 @@
 <?php 
 
+//Titleタグの有効化
 function setup_my_theme() {
     add_theme_support( 'title-tag' );
- }
- add_action( 'after_setup_theme', 'setup_my_theme');
+}
+add_action( 'after_setup_theme', 'setup_my_theme');
+
+//ログイン関数
+function login_init() {
+
+    //変数$errorのグローバル宣言
+    global $error;
+    $error = "";
+    $br ="<br/>";
+
+    //メールアドレスのチェック
+    if ( ! empty( $_POST['user_email'] ) ) {
+        $email = $_POST['user_email'];
+    } else {
+        $error .= "メールアドレスを入力してください{$br}";
+    }
+
+    //パスワードのチェック
+    if ( ! empty( $_POST['user_pass'] ) ) {
+        $password = $_POST['user_pass'];
+    } else {
+        $error .= "パスワードを入力してください{$br}";
+    }
+
+    //エラーが無ければログイン処理へ進む
+    if ( ! $error ) {
+        //ユーザー情報の取得
+        $user = get_user_by( 'email', $email );
+
+        //存在しないメールアドレスの場合
+        if ( false === $user ) {
+            $error .= "メールアドレスが間違っています。{$br}";
+
+        } else {
+            //ユーザー情報を配列に定義
+            $login = array();
+            $login['user_login'] = $user->data->user_login;
+            $login['user_password'] = $password;
+            $login['remember'] = true;
+
+            //ログインの実行
+            $user = wp_signon( $login, false );
+            
+            //ログイン失敗時の処理
+            if ( is_wp_error( $user ) ) {
+                if ( array_key_exists( 'incorrect_password', $user->errors ) ) {
+                    $error .= "パスワードが間違っています。{$br}";
+                } else {
+                    $error .= "ログインに失敗しました。{$br}";
+                }
+            
+            //ログイン成功時の処理
+            } else {
+                //ホーム画面へリダイレクト
+                wp_safe_redirect( home_url() );
+            }
+        }
+    }
+}
+add_action( 'template_redirect', 'login_init' );
 
 ?>
--- index.php
+++ index.php
@@ -8,7 +8,18 @@
 <body>
 <header>HEADER</header>
 <main>
-    
+    <h1>ログイン</h1>
+    <form action="<?php the_permalink(); ?>" method="post">
+        <?php if ( ! empty( $error ) ): ?>
+            <!-- エラー発生時にメッセージを表示 -->
+            <p><?php echo $error; ?></p>
+        <?php endif; ?>
+        <label for="Email">メールアドレス</label>
+        <input id="Email" type="email" name="user_email" value="">
+        <label for="Password">パスワード</label>
+        <input id="Password" type="password" name="user_pass">
+        <button type="submit">ログイン</button>
+    </form>
 </main>
 <footer>FOOTER</footer>
 <?php wp_footer(); ?>
