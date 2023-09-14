935e87179db7179d6e39fab2d83729e17907a956
umekochan <masuda.ff@gmail.com>
2023-09-14 15:15:42 +0900

    投稿フォームを作る

---
+++ page-post.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+<main>
+    <h1>投稿フォーム</h1>
+    <?php global $post_error; var_dump($post_error); ?>
+    <form action="<?php the_permalink(); ?>" method="post">
+        <div class="title-wrap">
+            <label for="title">タイトル</label>
+            <input type="text" name="title" id="title">
+        </div>
+        <div class="content-wrap">
+            <label for="content">本文</label>
+            <textarea name="content" id="content" cols="30" rows="10"></textarea>    
+        </div>
+        <button type="submit">投稿</button>
+    </form>
+</main>
+<?php get_footer(); ?>
