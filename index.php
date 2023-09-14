7acef59adb8a465296cb269339d35707fb032a9f
umekochan <masuda.ff@gmail.com>
2023-09-14 13:34:30 +0900

    indexに投稿一覧を設置

--- index.php
+++ index.php
@@ -8,7 +8,16 @@
 <body>
 <header>HEADER</header>
 <main>
-    <h1>index</h1>
+    <h1>投稿一覧</h1>
+    <?php if( have_posts() ): ?>
+        <?php while( have_posts() ): the_post(); ?>
+            <hr>
+            <article>
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <p>投稿日：<time datetime="<?php the_time('Y-m-j'); ?>"><?php the_time('Y年n月j日'); ?></time></p>
+            </article>
+        <?php endwhile; ?>
+    <?php endif; ?>
 </main>
 <footer>FOOTER</footer>
 <?php wp_footer(); ?>
