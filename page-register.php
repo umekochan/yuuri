2cce9897802ce5d8d0c257680e4304193b4e9b01
umekochan <masuda.ff@gmail.com>
2023-09-24 04:57:15 +0900

    コーディング完了

--- page-register.php
+++ page-register.php
@@ -1,22 +1,30 @@
-<?php get_header(); ?>
+<?php 
+    global $register_error;
+    get_header();
+?>
 <main>
     <h1>会員登録</h1>
-    <?php if ( !empty( $regi_error ) ): ?>
+    <?php if ( !empty( $register_error ) ): ?>
         <!-- エラー発生時にメッセージを表示 -->
-        <p><?php echo $regi_error; ?></p>
+        <p><?= $register_error; ?></p>
     <?php endif; ?>
+    <p>全て入力必須</p>
     <form action="<?php the_permalink(); ?>" method="post">
         <div class="wrap">
-            <label for="name">ユーザー名</label>
-            <input type="text" name="name" id="name" required>
+            <label for="register_id">ユーザーID　※半角英数のみ</label>
+            <input type="text" name="register_id" id="register_id" pattern="^[a-zA-Z0-9]+$" required>
         </div>
         <div class="wrap">
-            <label for="password">パスワード</label>
-            <input type="password" name="password" id="password" required>
+            <label for="register_name">ユーザー名（表示名）※全角可</label>
+            <input type="text" name="register_name" id="register_name" required>
         </div>
         <div class="wrap">
-            <label for="email">メールアドレス</label>
-            <input type="email" name="email" id="email" required>
+            <label for="register_pass">パスワード</label>
+            <input type="password" name="register_pass" id="register_pass" required>
+        </div>
+        <div class="wrap">
+            <label for="register_email">メールアドレス</label>
+            <input type="email" name="register_email" id="register_email" required>
         </div>
         <button type="submit">登録</button>
     </form>
